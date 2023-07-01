cef2ba06d985d424b2d1d2cdce2691a4a0692a0c
Pranav Choudhary <pranavchoudhary500@gmail.local>
2023-07-01 17:34:15 +0530

    Embarking on an epic journey 🚀

---
+++ CardWorkout-Programmatic/CardSelectionVC.swift
@@ -0,0 +1,85 @@
+//
+//  CardSelectionVC.swift
+//  CardWorkout-Programmatic
+//
+//  Created by Pranav Choudhary on 01/07/23.
+//
+
+import UIKit
+
+class CardSelectionVC: UIViewController {
+
+    let cardImageView = UIImageView()
+    let stopButton    = CWButton(backgroundColor: .red,title: "Stop")
+    let restartButton = CWButton(backgroundColor: .green,title: "Restart")
+    let rulesButton   = CWButton(backgroundColor: .blue,title: "Rules")
+    let allCards      = Card.allValues
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        configureUI()
+        
+    }
+    
+    
+    func configureUI(){
+        configureCardImageView()
+        configureStopButton()
+        configureRestartButton()
+        configureRulesButton()
+    }
+    
+    func configureCardImageView(){
+        view.addSubview(cardImageView)
+        cardImageView.translatesAutoresizingMaskIntoConstraints = false
+        cardImageView.image = allCards.randomElement()
+    
+        NSLayoutConstraint.activate([
+            cardImageView.widthAnchor.constraint(equalToConstant: 250),
+            cardImageView.heightAnchor.constraint(equalToConstant: 350),
+            cardImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            cardImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -75)
+        ])
+    }
+    
+    func configureStopButton(){
+        view.addSubview(stopButton)
+        
+        NSLayoutConstraint.activate([
+            stopButton.widthAnchor.constraint(equalToConstant: 260),
+            stopButton.heightAnchor.constraint(equalToConstant: 50),
+            stopButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stopButton.topAnchor.constraint(equalTo: cardImageView.bottomAnchor, constant: 30)
+        ])
+    }
+    
+    func configureRestartButton(){
+        view.addSubview(restartButton)
+        
+        NSLayoutConstraint.activate([
+            restartButton.widthAnchor.constraint(equalToConstant: 115),
+            restartButton.heightAnchor.constraint(equalToConstant: 50),
+            restartButton.leadingAnchor.constraint(equalTo: stopButton.leadingAnchor),
+            restartButton.topAnchor.constraint(equalTo: stopButton.bottomAnchor, constant: 30)
+        ])
+    }
+    
+    func configureRulesButton(){
+        view.addSubview(rulesButton)
+        NSLayoutConstraint.activate([
+            rulesButton.widthAnchor.constraint(equalToConstant: 115),
+            rulesButton.heightAnchor.constraint(equalToConstant: 50),
+            rulesButton.trailingAnchor.constraint(equalTo: stopButton.trailingAnchor),
+            rulesButton.topAnchor.constraint(equalTo: stopButton.bottomAnchor, constant: 30)
+        ])
+    }
+    
+    
+    
+    
+    
+    
+    
+
+}
